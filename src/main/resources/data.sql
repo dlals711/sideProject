892ba5f58bea99852bb365f099cc3116bf213ea9
leo <dlals711@g.yju.ac.kr>
2024-08-13 00:53:14 +0900

    관리자페이지 20240813

---
+++ src/main/resources/data.sql
@@ -0,0 +1,132 @@
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals711@naver.com', 'THIRD', '이영민1', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals712@naver.com', 'THIRD', '이영민2', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals713@naver.com', 'THIRD', '이영민3', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals711@naver.com', 'THIRD', '이영민1', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals712@naver.com', 'THIRD', '이영민2', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals713@naver.com', 'THIRD', '이영민3', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals711@naver.com', 'THIRD', '이영민1', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals712@naver.com', 'THIRD', '이영민2', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals713@naver.com', 'THIRD', '이영민3', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals711@naver.com', 'THIRD', '이영민1', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals712@naver.com', 'THIRD', '이영민2', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals713@naver.com', 'THIRD', '이영민3', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals711@naver.com', 'THIRD', '이영민1', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals712@naver.com', 'THIRD', '이영민2', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals713@naver.com', 'THIRD', '이영민3', 'dldudals00!', '01099356199', 'USER');
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals711@naver.com', 'THIRD', '이영민1', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals712@naver.com', 'THIRD', '이영민2', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals713@naver.com', 'THIRD', '이영민3', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals711@naver.com', 'THIRD', '이영민1', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals712@naver.com', 'THIRD', '이영민2', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals713@naver.com', 'THIRD', '이영민3', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals711@naver.com', 'THIRD', '이영민1', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals712@naver.com', 'THIRD', '이영민2', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals713@naver.com', 'THIRD', '이영민3', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals711@naver.com', 'THIRD', '이영민1', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals712@naver.com', 'THIRD', '이영민2', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals713@naver.com', 'THIRD', '이영민3', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals711@naver.com', 'THIRD', '이영민1', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals712@naver.com', 'THIRD', '이영민2', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals713@naver.com', 'THIRD', '이영민3', 'dldudals00!', '01099356199', 'USER');
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals711@naver.com', 'THIRD', '이영민1', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals712@naver.com', 'THIRD', '이영민2', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals713@naver.com', 'THIRD', '이영민3', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals711@naver.com', 'THIRD', '이영민1', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals712@naver.com', 'THIRD', '이영민2', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals713@naver.com', 'THIRD', '이영민3', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals711@naver.com', 'THIRD', '이영민1', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals712@naver.com', 'THIRD', '이영민2', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals713@naver.com', 'THIRD', '이영민3', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals711@naver.com', 'THIRD', '이영민1', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals712@naver.com', 'THIRD', '이영민2', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals713@naver.com', 'THIRD', '이영민3', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals711@naver.com', 'THIRD', '이영민1', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals712@naver.com', 'THIRD', '이영민2', 'dldudals00!', '01099356199', 'USER');
+
+insert into member(change_date, reg_date, email, grade, name, password, phone_number, role)
+values(NOW(), NOW(), 'dlals713@naver.com', 'THIRD', '이영민3', 'dldudals00!', '01099356199', 'USER');
