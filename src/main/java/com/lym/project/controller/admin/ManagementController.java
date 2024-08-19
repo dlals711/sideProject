e2cfba9f6be491e31fc6e5604b23791e985200c3
leo <dlals711@g.yju.ac.kr>
2024-08-20 02:01:53 +0900

    관리자페이지 20240820

--- src/main/java/com/lym/project/controller/admin/ManagementController.java
+++ src/main/java/com/lym/project/controller/admin/ManagementController.java
@@ -5,13 +5,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,19 +23,5 @@ public class ManagementController {
         modelAndView.setViewName("admin/management");
 
         return modelAndView;
-    }
-
-    @GetMapping("/content2")
-    public String content1(Model model) {
-        List<String> items = Arrays.asList("Item 1", "Item 2", "Item 3");
-        model.addAttribute("items", items);
-        model.addAttribute("content2", "This is content 1");
-        return "admin/content :: contentFragment2";
-    }
-
-    @GetMapping("/content3")
-    public String content2(Model model) {
-        model.addAttribute("content3", "This is content 2");
-        return "admin/content :: contentFragment3";
     }
 }
--- src/main/java/com/lym/project/controller/admin/MemberManagementController.java
+++ src/main/java/com/lym/project/controller/admin/MemberManagementController.java
@@ -3,12 +3,23 @@ package com.lym.project.controller.admin;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/admin")
 public class MemberManagementController {
-
+    @GetMapping("/memberManagement")
+    public String content1(Model model) {
+        List<String> items = Arrays.asList("Item 1", "Item 2", "Item 3");
+        model.addAttribute("items", items);
+        model.addAttribute("content2", "This is content 1");
+        return "admin/memberManagement :: contentFragment2";
+    }
 }
